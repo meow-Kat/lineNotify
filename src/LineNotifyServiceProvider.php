b5c37fc6a93e52b7cce0cb1fe0e95bce55baa208
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2023-06-09 15:23:30 +0800

    Update LineNotifyServiceProvider.php

--- src/LineNotifyServiceProvider.php
+++ src/LineNotifyServiceProvider.php
@@ -13,7 +13,6 @@ class LineNotifyServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // 如果 root 有 config 設定檔就合併，沒有就產生 config 設定檔，來源在 src/config
         $this->mergeConfigFrom( __DIR__ . '/config/line-notify.php', 'line-notify');
     }
 
@@ -24,15 +23,11 @@ class LineNotifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // 發佈 config 設定檔
-        // php artisan vendor:publish --tag=line-notify-config
         $source = realpath($raw = __DIR__ . '/config/line-notify.php') ?: $raw;
         $this->publishes([
             $source => config_path('line-notify.php'),
         ],'line-notify-config');
-
-        // 發佈 migration
-        // php artisan vendor:publish --tag=line-notify-migration
+        
         $timestamp = date('Y_m_d_His');
         $this->publishes([
             __DIR__.'/database/migrations/add_line_token_column_and_line_state_column_to_users_table.php' => database_path('migrations/'.$timestamp.'_add_line_token_column_and_line_state_token_to_users_table.php'),
